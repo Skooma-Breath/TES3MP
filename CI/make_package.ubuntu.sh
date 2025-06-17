f48dd657e677fce0cbb46566d68f5cd8a0d5bb2b
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2025-06-17 00:38:12 -0500

    Update make_package.ubuntu.sh

--- CI/make_package.ubuntu.sh
+++ CI/make_package.ubuntu.sh
@@ -3,9 +3,9 @@
 set -e
 
 PACKAGE_BINARIES=( \
-                   "dreamweave" \
-                       "dreamweave-browser" \
-                       "dreamweave-server" \
+                   "tes3mp" \
+                       "tes3mp-browser" \
+                       "tes3mp-server" \
                        "openmw-launcher" \
                        "openmw-wizard" \
                        "openmw-iniimporter" \
@@ -52,17 +52,17 @@ DEFAULTS=( \
            "defaults.bin" \
                "openmw.cfg" \
                "gamecontrollerdb.txt" \
-               "dreamweave-client-default.cfg" \
-               "dreamweave-server-default.cfg" \
+               "tes3mp-client-default.cfg" \
+               "tes3mp-server-default.cfg" \
                "tes3mp-credits.md" \
                "tes3mp-changelog.md" \
-               "dreamweave-credits.md" \
-               "dreamweave-changelog.md" \
+               "tes3mp-credits.md" \
+               "tes3mp-changelog.md" \
                "AUTHORS.md" \
                "LICENSE" \
     )
 
-mkdir dreamweave-build dreamweave-build/lib/ && mv resources/ dreamweave-build/ && cd dreamweave-build
+mkdir tes3mp-build tes3mp-build/lib/ && mv resources/ tes3mp-build/ && cd tes3mp-build
 
 for LIB in "${LIBRARIES[@]}"; do
     find /lib /usr/lib /usr/local/lib /usr/local/lib64 /lib/x86_64-linux-gnu/ ../raknet/ -name "$LIB*" -exec cp -r --preserve=links "{}" ./lib \; 2> /dev/null || true
@@ -77,14 +77,14 @@ for BIN in "${PACKAGE_BINARIES[@]}"; do
     cp -r ../$BIN .
 done
 
-git clone https://github.com/DreamWeave-MP/CoreScripts.git server/
+git clone https://github.com/tes3mp-MP/CoreScripts.git server/
 
 echo "Acquiring cjson . . ."
-curl  -o server/lib/cjson.dll -L https://github.com/DreamWeave-MP/lua-cjson/releases/download/Stable-CI/cjson-MinSizeRel.dll
+curl  -o server/lib/cjson.dll -L https://github.com/tes3mp-MP/lua-cjson/releases/download/Stable-CI/cjson-MinSizeRel.dll
 
 
 # Create pre-launch script
-cat << 'EOF' > dreamweave-prelaunch
+cat << 'EOF' > tes3mp-prelaunch
 #!/bin/bash
 
 ARGS="$*"
@@ -94,15 +94,15 @@ TES3MP_HOME="$HOME/.config/openmw"
 # If there are config files in the home directory, load those
 # Otherwise check the package/installation directory and load those
 # Otherwise copy them to the home directory
-if [[ "$ARGS" = 'dreamweave-server' ]]; then
-    if [[ -f "$TES3MP_HOME"/dreamweave-server.cfg ]]; then
+if [[ "$ARGS" = 'tes3mp-server' ]]; then
+    if [[ -f "$TES3MP_HOME"/tes3mp-server.cfg ]]; then
         echo -e "Loading server config from the home directory"
         LOADING_FROM_HOME=true
-    elif [[ -f "$GAMEDIR"/dreamweave-server-default.cfg ]]; then
+    elif [[ -f "$GAMEDIR"/tes3mp-server-default.cfg ]]; then
         echo -e "Loading server config from the package directory"
     else
         echo -e "Server config not found in home and package directory, trying to copy from .example"
-        cp -f dreamweave-server-default.cfg.example "$TES3MP_HOME"/dreamweave-server.cfg
+        cp -f tes3mp-server-default.cfg.example "$TES3MP_HOME"/tes3mp-server.cfg
         LOADING_FROM_HOME=true
     fi
     if [[ $LOADING_FROM_HOME ]]; then
@@ -111,17 +111,17 @@ if [[ "$ARGS" = 'dreamweave-server' ]]; then
         else
             echo -e "CoreScripts folder not found in home directory, copying from package directory"
             cp -rf "$GAMEDIR"/server/ "$TES3MP_HOME"/
-            sed -i "s|home = .*|home = $TES3MP_HOME/server |g" "$TES3MP_HOME"/dreamweave-server.cfg
+            sed -i "s|home = .*|home = $TES3MP_HOME/server |g" "$TES3MP_HOME"/tes3mp-server.cfg
         fi
     fi
 else
-    if [[ -f $TES3MP_HOME/dreamweave-client.cfg ]]; then
+    if [[ -f $TES3MP_HOME/tes3mp-client.cfg ]]; then
         echo -e "Loading client config from the home directory"
-    elif [[ -f dreamweave-client-default.cfg ]]; then
+    elif [[ -f tes3mp-client-default.cfg ]]; then
         echo -e "Loading client config from the package directory"
     else
         echo -e "Client config not found in home and package directory, trying to copy from .example"
-        cp -f "$GAMEDIR"/dreamweave-client-default.cfg.example "$TES3MP_HOME"/dreamweave-client.cfg
+        cp -f "$GAMEDIR"/tes3mp-client-default.cfg.example "$TES3MP_HOME"/tes3mp-client.cfg
     fi
 fi
 EOF
@@ -135,11 +135,11 @@ EOF
       WRAPPER="$BINARY"
       BINARY_RENAME="$BINARY.x86_64"
       mv "$BINARY" "$BINARY_RENAME"
-      printf "#!/bin/bash\n\nWRAPPER=\"\$(basename \$0)\"\nGAMEDIR=\"\$(dirname \$0)\"\ncd \"\$GAMEDIR\"\nif test -f ./dreamweave-prelaunch; then bash ./dreamweave-prelaunch \"\$WRAPPER\"; fi\nLD_LIBRARY_PATH=\"./lib\" ./$BINARY_RENAME \"\$@\"" > "$WRAPPER"
+      printf "#!/bin/bash\n\nWRAPPER=\"\$(basename \$0)\"\nGAMEDIR=\"\$(dirname \$0)\"\ncd \"\$GAMEDIR\"\nif test -f ./tes3mp-prelaunch; then bash ./tes3mp-prelaunch \"\$WRAPPER\"; fi\nLD_LIBRARY_PATH=\"./lib\" ./$BINARY_RENAME \"\$@\"" > "$WRAPPER"
     fi
   done
   chmod 755 *
 
   echo "Job complete! Making archive . . ."
   cd ..
-  tar -cvf dreamweave_linux.tar dreamweave-build
+  tar -cvf tes3mp_linux.tar tes3mp-build
