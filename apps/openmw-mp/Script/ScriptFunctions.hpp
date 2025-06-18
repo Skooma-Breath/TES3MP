a904e2cd5e4f2577451fddaa91187887e6598eb8
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2025-06-18 04:46:45 -0500

    Update ScriptFunctions.hpp

--- apps/openmw-mp/Script/ScriptFunctions.hpp
+++ apps/openmw-mp/Script/ScriptFunctions.hpp
@@ -116,8 +116,8 @@ public:
     */
     static bool IsTimerElapsed(int timerId) noexcept;
 
-    //changed to const...
-    static const ScriptFunctionData functions[] {
+
+    static constexpr ScriptFunctionData functions[]{
             {"CreateTimer",         ScriptFunctions::CreateTimer},
             {"CreateTimerEx",       ScriptFunctions::CreateTimerEx},
             {"MakePublic",          ScriptFunctions::MakePublic},
